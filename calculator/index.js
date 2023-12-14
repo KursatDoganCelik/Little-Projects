a73b5e1786a54694e22e197fd32eb978c6d7fab8
Kürşat Doğan Çelik <kursatdogancelik@gmail.com>
2023-12-14 16:55:04 +0300

    most of the part is finished

--- calculator/index.js
+++ calculator/index.js
@@ -8,18 +8,97 @@ clickListener('.number', numberBtnLength)
 function clickListener(selectorName, length) {
   for (let index = 0; index < length; index++) {
     document.querySelectorAll(selectorName)[index].addEventListener('click', function () {
-      console.log(this.className)
-      if (this.className === 'number') {
-        totalNumber(this.innerHTML)
+      if (selectorName === '.number') {
+        numberClick(this.innerHTML)
+      } else if (selectorName === '.operator') {
+        operatorClick(this.innerHTML)
       }
     })
   }
 }
 
-//add clicked number the last position
-let printedScreen = ''
-function totalNumber(clickedNumber) {
-  let currentScreenValue = document.querySelector('.screen')
-  printedScreen += clickedNumber
-  currentScreenValue.innerHTML = printedScreen
+//add number to last digit
+function numberClick(number) {
+  let mainScreen = document.querySelector('.main-screen')
+  let mainScreenValue = document.querySelector('.main-screen').innerHTML
+
+  //check multiple '.' usage
+  if (!mainScreenValue.includes('.') || number !== '.') {
+    if (mainScreenValue === '0' || mainScreenValue === '00') {
+      mainScreen.innerHTML = number
+    } else {
+      mainScreen.innerHTML += number
+    }
+  }
+}
+
+//apply to incoming operator
+let prevOperator
+function operatorClick(operator) {
+  let mainScreen = document.querySelector('.main-screen')
+  let bufferScreen = document.querySelector('.buffer-screen')
+  let mainScreenValue = document.querySelector('.main-screen').innerHTML
+  let bufferScreenValue = document.querySelector('.buffer-screen').innerHTML
+  let bufferValue = bufferScreenValue.slice(0, -2)
+
+  //check if AC (All Clear) operators used
+  if (operator === 'AC') {
+    mainScreen.innerHTML = '0'
+    bufferScreen.innerHTML = ''
+  }
+  //check if DEL (remove last) operators used
+  else if (operator === 'DEL') {
+    if (mainScreenValue.length === 1) {
+      mainScreen.innerHTML = 0
+    } else if (mainScreenValue.length > 1) {
+      mainScreen.innerHTML = mainScreenValue.slice(0, -1)
+    }
+  }
+  //check if CE (clear entry) operator used
+  else if (operator === 'CE') {
+    mainScreen.innerHTML = '0'
+  }
+  //check if '=' (equal) operators used
+  else if (operator === '=') {
+    if (bufferValue !== '' && mainScreenValue !== '') {
+      let calculatedValue = mathCalculations(bufferValue, mainScreenValue, prevOperator)
+      mainScreen.innerHTML = calculatedValue
+      bufferScreen.innerHTML = ''
+      prevOperator = ''
+    }
+  }
+  //other operator must be math operators so we need check main and buffer screen empty or not
+  else if (bufferValue !== '' && mainScreenValue !== '') {
+    let calculatedValue = mathCalculations(bufferValue, mainScreenValue, prevOperator)
+    bufferScreen.innerHTML = `${calculatedValue} ${operator}`
+    mainScreen.innerHTML = ''
+    prevOperator = operator
+  } else if (bufferValue === '' && mainScreenValue !== '') {
+    bufferScreen.innerHTML = `${mainScreenValue} ${operator}`
+    mainScreen.innerHTML = ''
+    prevOperator = operator
+  } else if (bufferValue !== '' && mainScreenValue === '') {
+    bufferScreen.innerHTML = `${bufferValue} ${operator}`
+    prevOperator = operator
+  }
+}
+
+//function for math operations
+function mathCalculations(numberOne, numberTwo, operator) {
+  numberOne = parseFloat(numberOne)
+  numberTwo = parseFloat(numberTwo)
+  switch (operator) {
+    case '+':
+      return Math.round((numberOne + numberTwo) * 100) / 100
+    case '-':
+      return Math.round((numberOne - numberTwo) * 100) / 100
+    case '*':
+      return Math.round(numberOne * numberTwo * 100) / 100
+    case '/':
+      return Math.round((numberOne / numberTwo) * 100) / 100
+    case '%':
+      return Math.round((numberOne % numberTwo) * 100) / 100
+    default:
+      console.log('Wrong Operator')
+  }
 }
